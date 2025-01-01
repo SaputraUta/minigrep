720a19a1e4c52af39f7738596a0c91718d77f527
Saputra <saputra.uta50@gmail.com>
2025-01-01 16:05:03 +0800

    closures example

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,48 @@
-use std::env;
-use std::process;
-use minigrep::Config;
+#[derive(Debug, PartialEq, Copy, Clone)]
+enum ShirtColor {
+    Red, Blue,
+}
 
-fn main() {
-    let args: Vec<String> = env::args().collect();
+struct Inventory {
+    shirts: Vec<ShirtColor>,
+}
 
-    let config = Config::build(&args).unwrap_or_else(|err| {
-        eprintln!("Problem parsing arguments: {}", err);
-        process::exit(1);
-    });
+impl Inventory {
+    fn giveaway(&self, user_preference: Option<ShirtColor>) -> ShirtColor {
+        user_preference.unwrap_or_else(|| self.most_stocked())
+    }
 
-    if let Err(e) = minigrep::run(config) {
-        eprintln!("Application error : {e}");
-        process::exit(1);
+    fn most_stocked(&self) -> ShirtColor {
+        let mut num_red = 0;
+        let mut num_blue = 0;
+
+        for color in &self.shirts {
+            match color {
+                ShirtColor::Red => num_red += 1,
+                ShirtColor::Blue => num_blue += 1,
+            }
+        }
+
+        if num_red > num_blue {
+            ShirtColor::Red
+        } else {
+            ShirtColor::Blue
+        }
     }
 }
 
+fn main() {
+    let store = Inventory {
+        shirts: vec![ShirtColor::Blue, ShirtColor::Red, ShirtColor::Blue],
+    };
+
+    let user_pref1 = Some(ShirtColor::Red);
+    let giveaway1 = store.giveaway(user_pref1);
+    println!("The user with preference {:?} gets {:?}", user_pref1, giveaway1);
+    
+    let user_pref2 = None;
+    let giveaway2 = store.giveaway(user_pref2);
+    println!("The user with preference {:?} gets {:?}", user_pref2, giveaway2);
+
+
+}
